7680b9f8d47e06e8122a483ea9dfcad088d7589e
Max Whittingham <max.whittingham@gmail.com>
2020-09-29 11:34:42 -0600

    WIP OSD-5065

--- pkg/e2e/verify/regularuser_webhook.go
+++ pkg/e2e/verify/regularuser_webhook.go
@@ -13,45 +13,105 @@ import (
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/openshift/osde2e/pkg/common/util"
 
+	autoscalingv1 "github.com/openshift/cluster-autoscaler-operator/pkg/apis/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
 
-var userWebhookTestName string = "[Suite: service-definition] [OSD] regularuser validating webhook"
-
 const (
-	SRE_PROVIDER_NAME      = "OpenShift_SRE"
-	CUSTOMER_PROVIDER_NAME = "CUSTOM"
+	NvidiaGPU          = "nvidia.com/gpu"
+	TestNamespace      = "test-namespace"
+	TestCloudProvider  = "testProvider"
+	TestReleaseVersion = "v100"
 )
 
+var (
+	ScaleDownUnneededTime        = "10s"
+	ScaleDownDelayAfterAdd       = "60s"
+	MaxNodeProvisionTime         = "30m"
+	PodPriorityThreshold   int32 = -10
+	MaxPodGracePeriod      int32 = 60
+	MaxNodesTotal          int32 = 100
+	CoresMin               int32 = 16
+	CoresMax               int32 = 32
+	MemoryMin              int32 = 32
+	MemoryMax              int32 = 64
+	NvidiaGPUMin           int32 = 4
+	NvidiaGPUMax           int32 = 8
+)
+
+var userWebhookTestName string = "[Suite: service-definition] [OSD] regularuser validating webhook"
+
 func init() {
-	alert.RegisterGinkgoAlert(userWebhookTestName, "SD-SREP", "Haoran Wang", "sd-cicd-alerts", "sd-cicd@redhat.com", 4)
+	alert.RegisterGinkgoAlert(userWebhookTestName, "SD-SREP", "Max Whittingham", "sd-cicd-alerts", "sd-cicd@redhat.com", 4)
 }
 
 var _ = ginkgo.Describe(userWebhookTestName, func() {
 	h := helper.New()
 
-	ginkgo.Context("user validating webhook", func() {
-		ginkgo.It("dedicated admins cannot manage redhat users", func() {
-			userName := util.RandomStr(5) + "@redhat.com"
+	ginkgo.Context("regularuser validating webhook", func() {
+		ginkgo.It("kube:system can create autoscalers", func() {
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+				h.Kube().AutoscalingV1().RESTClient().Delete().Resource(*autoscalingv1.ClusterAutoscaler) //TODO: Verify this removes the cluster autoscaler
+			}()
+
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "kube:system",
+				Groups: []string{
+					"system:authenticated",
+					"system:authenticated:oauth",
+				},
+			})
+			_, err := NewClusterAutoscaler()(*autoscalingv1.ClusterAutoscaler, err)
+			Expect(err).NotTo(HaveOccurred())
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		ginkgo.It("unpriv users cannot create autoscalers", func() {
+			userName := util.RandomStr(5) + "@customdomain"
 			user, err := createUser(userName, []string{}, h)
 			defer func() {
 				h.Impersonate(rest.ImpersonationConfig{})
 				deleteUser(user.Name, h)
+				h.Kube().AutoscalingV1().RESTClient().Delete() //HPA?
 			}()
 			Expect(err).NotTo(HaveOccurred())
 
 			h.Impersonate(rest.ImpersonationConfig{
 				UserName: "test@customdomain",
 				Groups: []string{
-					"dedicated-admins",
+					"system:authenticated",
+					"system:authenticated:oauth",
 				},
 			})
-			err = deleteUser(userName, h)
+			_, err = NewClusterAutoscaler()(*autoscalingv1.ClusterAutoscaler, err)
 			Expect(err).To(HaveOccurred())
 		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 
-		ginkgo.It("dedicated admins can manage customer users", func() {
+		ginkgo.It("system:node can update nodes", func() {
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "system:node:ip-10-0-0-1.test",
+				Groups: []string{
+					"system:nodes",
+					"system:authenticated",
+				},
+			})
+			_, err := h.CreateServiceAccounts().Kube().CoreV1().Nodes().Get() //How to "update" node object?
+			Expect(err).NotTo(HaveOccurred())
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		ginkgo.It("unauthenticated users cannot update nodes", func() {
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "system:node:ip-10-0-0-1.test",
+				Groups: []string{
+					"system:unauthenticated",
+				},
+			})
+			_, err := h.CreateServiceAccounts().Kube().CoreV1().Nodes().Update(ctx context.Context, node *v1.Node, opts metav1.UpdateOptions) (*v1.Node, error) //Todo : Fix node update?
+			Expect(err).To(HaveOccurred())
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+		
+		ginkgo.It("unpriv users cannot delete nodes", func() {
 			userName := util.RandomStr(5) + "@customdomain"
 			user, err := createUser(userName, []string{}, h)
 			defer func() {
@@ -63,54 +123,53 @@ var _ = ginkgo.Describe(userWebhookTestName, func() {
 			h.Impersonate(rest.ImpersonationConfig{
 				UserName: "test@customdomain",
 				Groups: []string{
-					"dedicated-admins",
+					"system:unauthenticated",
+				},
+			})
+			_, err = h.CreateServiceAccounts().Kube().CoreV1().Nodes().Delete(ctx context.Context, node *v1.Node, opts metav1.UpdateOptions) err // Todo: Verify node deletion
+			Expect(err).To(HaveOccurred())
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		ginkgo.It("osd-sre-admins can delete nodes", func() {
+			userName := util.RandomStr(5) + "@customdomain"
+			user, err := createUser(userName, []string{}, h)
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+				deleteUser(user.Name, h)
+			}()
+			Expect(err).NotTo(HaveOccurred())
+
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "someuser@customdomain",
+				Groups: []string{
+					"osd-sre-admins",
 					"system:authenticated",
+					"system:authenticated:oauth",
 				},
 			})
-			err = deleteUser(userName, h)
+			_, err = h.CreateServiceAccounts().Kube().CoreV1().Nodes().Delete(ctx context.Context, node *v1.Node, opts metav1.UpdateOptions) err // Todo: Verify node deletion
 			Expect(err).NotTo(HaveOccurred())
 		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 
-		ginkgo.It("dedicated admins cannot manage redhat user identity", func() {
-			providerUsername := util.RandomStr(5)
-			idName := SRE_PROVIDER_NAME + ":" + util.RandomStr(5)
-			identity, err := createIdentity(idName, SRE_PROVIDER_NAME, providerUsername, h)
+		ginkgo.It("osd-sre-admins can delete nodes", func() {
+			userName := util.RandomStr(5) + "@customdomain"
+			user, err := createUser(userName, []string{}, h)
 			defer func() {
 				h.Impersonate(rest.ImpersonationConfig{})
-				err = deleteIdentity(identity.Name, h)
-				Expect(err).NotTo(HaveOccurred())
+				deleteUser(user.Name, h)
 			}()
 			Expect(err).NotTo(HaveOccurred())
 
 			h.Impersonate(rest.ImpersonationConfig{
-				UserName: "test@customdomain",
+				UserName: "someuser@customdomain",
 				Groups: []string{
-					"dedicated-admins",
+					"osd-sre-admins",
+					"system:authenticated",
+					"system:authenticated:oauth",
 				},
 			})
-			err = deleteIdentity(idName, h)
-			Expect(err).To(HaveOccurred())
-		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
-
-		ginkgo.It("dedicated admins can manage customer user identity", func() {
-			providerUsername := util.RandomStr(5)
-			idName := CUSTOMER_PROVIDER_NAME + ":" + util.RandomStr(5)
-			identity, err := createIdentity(idName, CUSTOMER_PROVIDER_NAME, providerUsername, h)
-			defer func() {
-				h.Impersonate(rest.ImpersonationConfig{})
-				err = deleteIdentity(identity.Name, h)
-				Expect(err).NotTo(HaveOccurred())
-			}()
+			_, err = h.CreateServiceAccounts().Kube().CoreV1().Nodes().Delete(ctx context.Context, node *v1.Node, opts metav1.UpdateOptions) err // Todo: Verify node deletion
 			Expect(err).NotTo(HaveOccurred())
-
-			h.Impersonate(rest.ImpersonationConfig{
-				UserName: "test@customdomain",
-				Groups: []string{
-					"dedicated-admins",
-				},
-			})
-			err = deleteIdentity(idName, h)
-			Expect(err).To(HaveOccurred())
 		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 	})
 })
@@ -129,17 +188,43 @@ func deleteUser(userName string, h *helper.H) error {
 	return h.User().UserV1().Users().Delete(context.TODO(), userName, metav1.DeleteOptions{})
 }
 
-func createIdentity(idName string, providername string, providerUserName string, h *helper.H) (*userv1.Identity, error) {
-	identity := &userv1.Identity{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: idName,
+//Pulled from cluster-autoscaler-operator
+func NewClusterAutoscaler() *autoscalingv1.ClusterAutoscaler {
+	return &autoscalingv1.ClusterAutoscaler{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterAutoscaler",
+			APIVersion: "autoscaling.openshift.io/v1",
 		},
-		ProviderName:     providername,
-		ProviderUserName: providerUserName,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: TestNamespace,
+		},
+		Spec: autoscalingv1.ClusterAutoscalerSpec{
+			MaxPodGracePeriod:    &MaxPodGracePeriod,
+			PodPriorityThreshold: &PodPriorityThreshold,
+			ResourceLimits: &autoscalingv1.ResourceLimits{
+				MaxNodesTotal: &MaxNodesTotal,
+				Cores: &autoscalingv1.ResourceRange{
+					Min: CoresMin,
+					Max: CoresMax,
+				},
+				Memory: &autoscalingv1.ResourceRange{
+					Min: MemoryMin,
+					Max: MemoryMax,
+				},
+				GPUS: []autoscalingv1.GPULimit{
+					{
+						Type: NvidiaGPU,
+						Min:  NvidiaGPUMin,
+						Max:  NvidiaGPUMax,
+					},
+				},
+			},
+			ScaleDown: &autoscalingv1.ScaleDownConfig{
+				Enabled:       true,
+				DelayAfterAdd: &ScaleDownDelayAfterAdd,
+				UnneededTime:  &ScaleDownUnneededTime,
+			},
+		},
 	}
-	return h.User().UserV1().Identities().Create(context.TODO(), identity, metav1.CreateOptions{})
-}
-
-func deleteIdentity(idName string, h *helper.H) error {
-	return h.User().UserV1().Identities().Delete(context.TODO(), idName, metav1.DeleteOptions{})
 }
